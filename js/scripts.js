0a4aa938eca214160b7009cf056f68a1756d78c2
Gathua Kiragu <kiragugathua@gmail.com>
2017-02-26 17:24:36 +0300

    Edited the javascript file to plain javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,12 @@
-// user
-$(document).ready(function() {
-  $("form#pingPongForm").submit(function(event) {
-    $("#userOutput").empty();
-    var userInput = parseInt($("input#number").val());
-    var result = pingPongFun(userInput);
-    result.forEach(function(output) {
-    $("#userOutput").append("<li>" + output + "</li>");
-    event.preventDefault();
-  });
-});
-});
-
-//business
-function pingPongFun(userInput){
+function myFunction(){
+var input = "#number";
 var result = [];
-for (var i = 1; i <= userInput; i++){
-  if ((i % 15) === 0){
-    result.push ("Ping-Pong");
-  } else if ((i % 5) === 0){
-    result.push ("Pong");
-  } else if ((i % 3) === 0){
-    result.push ("Ping");
-  } else
-    result.push (i);
+for (var i = 1; i <=input; i++){
+if ((i % 15) === 0){
+document.write ("Ping-Pong");
+} else if ((i % 5) === 0){
+document.write("Pong");
+} else ((i % 3) === 0){
+document.write("Ping");
 }
-return result;
-}
+};
