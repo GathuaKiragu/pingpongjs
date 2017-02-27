b27bfa9f3a76c0004b01791da305f17a69b35429
Gathua Kiragu <kiragugathua@gmail.com>
2017-02-27 08:58:28 +0300

    added jquery and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,30 @@
-function myFunction(){
-var input = "#number";
-var result = [];
-for (var i = 1; i <=input; i++){
-if ((i % 15) === 0){
-document.write ("Ping-Pong");
-} else if ((i % 5) === 0){
-document.write("Pong");
-} else ((i % 3) === 0){
-document.write("Ping");
+
+function pingPong(number) {  
+  var result = [];
+  for (var i = 1; i <= number; i++){
+    if ((i % 15) === 0){
+      result.push("Ping-Pong");
+    } else if ((i % 5) === 0){
+      result.push("Pong");
+    } else if ((i % 3) === 0){
+      result.push("Ping");
+    } else {
+      result.push(i)
+    }
+  }
+  
+  return result;   
+
 }
-};
+
+$(document).ready(function() {
+  $("form#pingPongForm").submit(function(event) {
+    event.preventDefault();
+
+    var input = parseInt($("input#number").val());
+    var result = pingPong(input);
+    result.forEach(function(number) {
+      $("ul#result").append("<li>"+number+"</li>");
+    });
+  });
+});
